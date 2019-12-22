231852e91fca0246fd04627c5aac2ee17814a556
Aayush soni <A-a-y-u-s-h-s-o-n-i@github.com>
2019-12-23 00:17:19 +0530

    Updates

---
+++ precache-manifest.21f0b5be44a28e20a9dbaa9a56837ab4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a587c4e4a9b975fecde4b59f361de63",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9bbc8c4b811d820147f3",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0acc49a84dedd2435631",
+    "url": "/robofriends/static/css/main.61993e62.chunk.css"
+  },
+  {
+    "revision": "9bbc8c4b811d820147f3",
+    "url": "/robofriends/static/js/2.3892a034.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.3892a034.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0acc49a84dedd2435631",
+    "url": "/robofriends/static/js/main.d91f7abd.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
